d760ddcdda0aef3320b827722683416563c653a5
TianKai Ma <tiankaima@163.com>
2026-07-20 00:41:05 +0800

    fix(seo): localize catalog social metadata

--- src/routes/teachers/+page.server.ts
+++ src/routes/teachers/+page.server.ts
@@ -1,6 +1,18 @@
 import { getTeacherListPage } from "@/features/catalog/server/public-page-list-data";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ locals, url }) => {
-  return getTeacherListPage(url, locals.locale);
+export const load: PageServerLoad = async ({ locals, parent, url }) => {
+  const [data, layoutData] = await Promise.all([
+    getTeacherListPage(url, locals.locale),
+    parent(),
+  ]);
+
+  return {
+    ...data,
+    socialMetadata: {
+      ...layoutData.socialMetadata,
+      description: data.labels.teachers.subtitle,
+      title: `${data.labels.common.teachers} - Life@USTC`,
+    },
+  };
 };
